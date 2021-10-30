1087f1ef9008eaaed635c431b816c335267321dc
phamnhutnpx <xuannhutzz@gmail.com>
2021-10-30 11:28:37 +0700

    Nhut push code 30-10 sua va xoa danh muc

--- shopdienthoai/classes/category.php
+++ shopdienthoai/classes/category.php
@@ -39,8 +39,52 @@
         public function show_category()
         {
             $query = "SELECT * FROM tbl_category ORDER BY catId DESC";
-                $result = $this->db->insert($query);
+                $result = $this->db->select($query);
                 return $result;
+        }
+        public function getCatById($id)
+        {
+                        
+                $query = "SELECT * FROM tbl_category WHERE catId='$id'";
+                $result = $this->db->select($query);
+                return $result;
+            
+        }
+        public function updateCatgory($id,$catName){
+            //kiểm tra các kí tự nhập vào có hợp lệ không
+            $catName = $this->fm->validation($catName);
+            
+            //xử lí các kí tự đặc biệt có thể gây lỗi khi truy vấn
+            $catName = mysqli_real_escape_string($this->db->link,$catName);
+            $id      = mysqli_real_escape_string($this->db->link,$id);
+
+            if(empty($catName)){
+                $alert = " <span class='error'>Vui lòng không để trống danh mục</span>";
+                return $alert;
+            }else{
+                $query = "UPDATE tbl_category 
+                            SET catName = '$catName'
+                            WHERE catId = '$id'";
+                $updateRow = $this->db->update($query);
+                if($updateRow){
+                    $alert = "<span class='success'>Cập nhật thành công</span>";
+                    return $alert;
+                }else{
+                    $alert = "<span class='error'>Cập nhật thất bại</span>";
+                    return $alert;
+                }
+            }
+        }
+        public function delCatName($id){
+            $query = "DELETE FROM tbl_category WHERE catId = '$id'";
+            $delRow = $this->db->delete($query);
+            if($delRow){
+                $alert = "<span class='success'>Xóa thành công</span>";
+                    return $alert;
+            }else{
+                $alert = "<span class='success'>Xóa thất bại</span>";
+                    return $alert;
+            }
         }
     }
 ?>
