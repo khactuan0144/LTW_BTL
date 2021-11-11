c01f51baeb1e4a2f753fb226de37fd2eca329144
Khac Tuan <ktuan439@gmail.com>
2021-11-11 21:45:25 +0700

    cập nhật chức năng đăng nhập của user

--- shopdienthoai/inc/header.php
+++ shopdienthoai/inc/header.php
@@ -1,3 +1,38 @@
+<?php
+    include 'lib/session.php';
+    Session::init();
+?>
+<?php
+	
+	include 'lib/database.php';
+	include 'helpers/format.php';
+
+	spl_autoload_register(function($class){
+		include_once "classes/".$class.".php";
+	});
+		
+
+	$db = new Database();
+	$fm = new Format();
+	/* $ct = new cart();
+	$us = new user();
+	$br = new brand(); */
+	$cat = new category();
+	$cs = new customer();
+	//$product = new product();
+		
+	      	 	
+?>
+
+<?php
+  header("Cache-Control: no-cache, must-revalidate");
+  header("Pragma: no-cache"); 
+  header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); 
+  header("Cache-Control: max-age=2592000");
+?>
+
+
+
 
 <!DOCTYPE HTML>
 
--- shopdienthoai/login.php
+++ shopdienthoai/login.php
@@ -1,83 +1,86 @@
 <?php 
-	include'inc/header.php';
-	
+	include 'inc/header.php';
 ?>
 
-<div class="main">
+<?php
+   
+    if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['submit'])) {
+        
+        $insertCustomers = $cs->insert_customers($_POST);
+        
+    }
+?>
+<?php
+	if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['login'])) {
+        
+        $login_Customers = $cs->login_customers($_POST);
+        
+    }
+?>
+ <div class="main">
     <div class="content">
     	 <div class="login_panel">
-        	<h3>Đã có tài khoản</h3>
-        	<p>Đăng nhập bằng cách nhập thông tin bên dưới</p>
-        	<form action="hello" method="get" id="member">
-                	<input name="Domain" type="text" value="Username" class="field" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Username';}">
-                    <input name="Domain" type="password" value="Password" class="field" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Password';}">
-                 </form>
-                 <p class="note">Nếu bạn quên mật khẩu hãy nhấn vào <a href="#">đây</a></p>
-                    <div class="buttons"><div><button class="grey">Đăng nhập</button></div></div>
-                    </div>
+        	<h2>Đăng nhập</h2>
+        	<?php
+			if(isset($login_Customers)){
+    			echo $login_Customers;
+    		}
+        	?>
+        	<form action="" method="post">
+                	<input  type="text" name="email" class="field" placeholder="Email">
+                    <input  type="password" name="password" class="field"  placeholder="Mật khẩu" >
+                 
+                 <p class="note"><a href="#">Quên mật khẩu</a></p>
+                    <div class="buttons"><div><input type="submit" name="login" class="grey" value="Đăng nhập"></div></div>
+             </form>
+          </div>
+         <?php
+
+         ?> 
     	<div class="register_account">
-    		<h3>Đăng ký tài khoản</h3>
-    		<form>
+    		<h3>Đăng kí tài khoản mới</h3>
+    		<?php
+    		if(isset($insertCustomers)){
+    			echo $insertCustomers;
+    		}
+    		?>
+    		<form action="" method="POST">
 		   			 <table>
 		   				<tbody>
 						<tr>
 						<td>
 							<div>
-							<input type="text" value="Name" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Tên';}" >
+							<input type="text" name="name" placeholder="Tên" >
 							</div>
 							
 							<div>
-							   <input type="text" value="City" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Tỉnh/Thành phố';}">
-							</div>
-							
-							<div>
-								<input type="text" value="Zip-Code" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Mã';}">
-							</div>
-							<div>
-								<input type="text" value="E-Mail" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Địa chỉ E-Mail';}">
+								<input type="text" name="email"  placeholder="Email"  >
 							</div>
 		    			 </td>
 		    			<td>
 						<div>
-							<input type="text" value="Address" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Địa chỉ';}">
+							<input type="text" name="address"  placeholder="Địa chỉ"  >
 						</div>
-		    		<div>
-						<select id="country" name="country" onchange="change_country(this.value)" class="frm-field required">
-							<option value="null">Chọn quốc gia</option>         
-							<option value="AF">Afghanistan</option>
-							<option value="AL">Albania</option>
-							<option value="DZ">Algeria</option>
-							<option value="AR">Argentina</option>
-							<option value="AM">Armenia</option>
-							<option value="AW">Aruba</option>
-							<option value="AU">Australia</option>
-							<option value="AT">Austria</option>
-							<option value="AZ">Azerbaijan</option>
-							<option value="BS">Bahamas</option>
-							<option value="BH">Bahrain</option>
-							<option value="BD">Bangladesh</option>
-
-		         </select>
-				 </div>		        
 	
 		           <div>
-		          <input type="text" value="Phone" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Phone';}">
+		          <input type="text" name="phone"  placeholder="Số điện thoại" >
 		          </div>
 				  
 				  <div>
-					<input type="text" value="Password" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Password';}">
+					<input type="text" name="password"  placeholder="Mật khẩu" >
 				</div>
 		    	</td>
 		    </tr> 
 		    </tbody></table> 
-		   <div class="search"><div><button class="grey">Tạo tài khoản</button></div></div>
-		    <p class="terms">Bằng các nhấn vào "Tạo tài khoản" bạn đã chấp nhận <a href="#">Điều khoản &amp; Điều kiện</a>.</p>
+		   <div class="search"><div><input type="submit" name="submit" class="grey" value="Tạo tài khoản"></div></div>
+		    
 		    <div class="clear"></div>
 		    </form>
     	</div>  	
        <div class="clear"></div>
     </div>
  </div>
- <?php
-include'inc/footer.php';
-?>
+<?php 
+	include 'inc/footer.php';
+	
+ ?>
